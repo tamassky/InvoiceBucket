0332cf2a28d37c3c7509087c2439e9e52be2f344
Kovacs Tamas <openbvehu@gmail.com>
2020-02-19 17:07:15 +0000

    working on InvoiceData generator

--- request/InvoiceData.js
+++ request/InvoiceData.js
@@ -11,6 +11,7 @@ function buildInvoiceData(data){
 	invoicedata = invoicedata + '<invoiceMain>';
 	invoicedata = invoicedata + '<invoice>';
 	
+	/*REFERENCE*/
 	if(invoiceOperation != 'CREATE'){
 		invoicedata = invoicedata + '<invoiceReference>';
 		invoicedata = invoicedata + '<originalInvoiceNumber>' + data.originalInvoiceNumber +'</originalInvoiceNumber>';
@@ -21,6 +22,7 @@ function buildInvoiceData(data){
 	
 	invoicedata = invoicedata + '<invoiceHead>';
 	
+	/*SUPPLIER*/
 	invoicedata = invoicedata + '<supplierInfo>';
 	invoicedata = invoicedata + '<supplierTaxNumber>';
 	invoicedata = invoicedata + '<taxpayerId>' + data.supplierTaxpayerId + '</taxpayerId>';
@@ -42,6 +44,7 @@ function buildInvoiceData(data){
 	invoicedata = invoicedata + '<individualExemption>' + data.individualExemption + '</individualExemption>';
 	invoicedata = invoicedata + '</supplierInfo>';
 	
+	/*CUSTOMER*/
 	invoicedata = invoicedata + '<customerInfo>';
 	if(data.customerTaxpayerId){
 		invoicedata = invoicedata + '<customerTaxNumber>';
@@ -64,6 +67,7 @@ function buildInvoiceData(data){
 	}
 	invoicedata = invoicedata + '</customerInfo>';
 	
+	/*INVOICE DETAILS*/
 	invoicedata = invoicedata + '<invoiceDetail>';
 	invoicedata = invoicedata + '<invoiceCategory>' + data.invoiceCategory + '</invoiceCategory>';
 	invoicedata = invoicedata + '<invoiceDeliveryDate>' + data.invoiceDeliveryDate + '</invoiceDeliveryDate>';
@@ -84,7 +88,101 @@ function buildInvoiceData(data){
 	invoicedata = invoicedata + '<invoiceAppearance>' + data.invoiceAppearance + '</invoiceAppearance>';
 	invoicedata = invoicedata + '</invoiceDetail>';
 	
-	/*CONTINUE HERE*/
+	/*LINES*/
+	invoicedata = invoicedata + '<invoiceLines>';
+	for(var i=0; i<data.lines.length; i++){
+		invoicedata = invoicedata + '<line>';
+		invoicedata = invoicedata + '<lineNumber>' + data.lines[i].lineNumber + '</lineNumber>';
+		if(data.lines[i].lineNumberReference){
+			invoicedata = invoicedata + '<lineModificationReference>';
+			invoicedata = invoicedata + '<lineNumberReference>' + data.lines[i].lineNumberReference + '</lineNumberReference>';
+			invoicedata = invoicedata + '<lineOperation>' + data.lines[i].lineOperation + '</lineOperation>';
+			invoicedata = invoicedata + '</lineModificationReference>';
+		}
+		if(data.lines[i].productCodeValue){
+		invoicedata = invoicedata + '<productCodes>';
+		invoicedata = invoicedata + '<productCode>';
+		invoicedata = invoicedata + '<productCodeCategory>' + data.lines[i].productCodeCategory + '</productCodeCategory>';
+		if(data.lines[i].productCodeCategory == 'OWN'){
+			invoicedata = invoicedata + '<productCodeOwnValue>' + data.lines[i].productCodeValue + '</productCodeOwnValue>';
+		}
+		else{
+			invoicedata = invoicedata + '<productCodeValue>' + data.lines[i].productCodeValue + '</productCodeValue>';
+		}
+		invoicedata = invoicedata + '</productCode>';
+		invoicedata = invoicedata + '</productCodes>';
+		}
+		if(data.lines[i].lineNatureIndicator){
+			invoicedata = invoicedata + '<lineNatureIndicator>' + data.lines[i].lineNatureIndicator + '</lineNatureIndicator>';
+		}
+		invoicedata = invoicedata + '<lineDescription>' + data.lines[i].lineDescription + '</lineDescription>';
+		invoicedata = invoicedata + '<quantity>' + data.lines[i].quantity + '.00</quantity>';
+		invoicedata = invoicedata + '<unitOfMeasure>' + data.lines[i].unitOfMeasure + '</unitOfMeasure>';
+		if(data.lines[i].unitOfMeasure == 'OWN'){
+			invoicedata = invoicedata + '<unitOfMeasureOwn>' + data.lines[i].unitOfMeasureOwn + '</unitOfMeasureOwn>';
+		}
+		invoicedata = invoicedata + '<unitPrice>' + data.lines[i].unitPrice + '.00</unitPrice>';
+		invoicedata = invoicedata + '<unitPriceHUF>' + data.lines[i].unitPrice + '.00</unitPriceHUF>';
+		if(data.lines[i].intermediatedService){
+		invoicedata = invoicedata + '<intermediatedService>' + data.lines[i].intermediatedService + '</intermediatedService>';
+		}
+		if(data.invoiceCategory == 'NORMAL'){
+			invoicedata = invoicedata + '<lineAmountsNormal>';
+			invoicedata = invoicedata + '<lineNetAmountData>';
+			invoicedata = invoicedata + '<lineNetAmount>' + data.lines[i].lineNetAmount + '.00</lineNetAmount>';
+			invoicedata = invoicedata + '<lineNetAmountHUF>' + data.lines[i].lineNetAmount + '.00</lineNetAmountHUF>';
+			invoicedata = invoicedata + '</lineNetAmountData>';
+			invoicedata = invoicedata + '<lineVatRate>';
+			invoicedata = invoicedata + '<vatPercentage>' + (data.lines[i].vatPercentage / 100).toString() + '</vatPercentage>';
+			invoicedata = invoicedata + '</lineVatRate>';
+			invoicedata = invoicedata + '<lineVatData>';
+			invoicedata = invoicedata + '<lineVatAmount>' + data.lines[i].lineVatAmount + '.00</lineVatAmount>';
+			invoicedata = invoicedata + '<lineVatAmountHUF>' + data.lines[i].lineVatAmount + '.00</lineVatAmountHUF>';
+			invoicedata = invoicedata + '</lineVatData>';
+			invoicedata = invoicedata + '<lineGrossAmountData>';
+			invoicedata = invoicedata + '<lineGrossAmountNormal>' + (data.lines[i].lineNetAmount + data.lines[i].lineVatAmount) + '.00</lineGrossAmountNormal>';
+			invoicedata = invoicedata + '<lineGrossAmountNormalHUF>' + (data.lines[i].lineNetAmount + data.lines[i].lineVatAmount) + '.00</lineGrossAmountNormalHUF>';
+			invoicedata = invoicedata + '</lineGrossAmountData>';
+			invoicedata = invoicedata + '</lineAmountsNormal>';
+		}
+		if(data.invoiceCategory == 'SIMPLIFIED'){
+			invoicedata = invoicedata + '<lineAmountsSimplified>';
+			invoicedata = invoicedata + '<lineVatContent>' +  (data.lines[i].lineVatContentSimplified / 100).toString() + '</lineVatContent>';
+			invoicedata = invoicedata + '<lineGrossAmountSimplified>' + data.lines[i].lineGrossAmountSimplified + '.00</lineGrossAmountSimplified>';
+			invoicedata = invoicedata + '<lineGrossAmountSimplifiedHUF>' + data.lines[i].lineGrossAmountSimplified + '.00</lineGrossAmountSimplifiedHUF>';
+			invoicedata = invoicedata + '</lineAmountsSimplified>';
+		}
+		invoicedata = invoicedata + '</line>';
+	}
+	invoicedata = invoicedata + '</invoiceLines>';
+	
+	/*SUMMARY*/
+	invoicedata = invoicedata + '<invoiceSummary>';
+	if(data.invoiceCategory == 'NORMAL'){
+		invoicedata = invoicedata + '<summaryNormal>';
+		for(var i=0; i<data.vatRates.length; i++){
+			invoicedata = invoicedata + '<summaryByVatRate>';
+			/*INDEV!!!!!!!!!!!!!!*/
+			invoicedata = invoicedata + '</summaryByVatRate>';
+		}
+		invoicedata = invoicedata + '<invoiceNetAmount>' + data.invoiceNetAmount + '.00</invoiceNetAmount>';
+		invoicedata = invoicedata + '<invoiceNetAmountHUF>' + data.invoiceNetAmount + '.00</invoiceNetAmountHUF>';
+		invoicedata = invoicedata + '<invoiceVatAmount>' + data.invoiceVatAmount + '.00</invoiceVatAmount>';
+		invoicedata = invoicedata + '<invoiceVatAmountHUF>' + data.invoiceVatAmount + '.00</invoiceVatAmountHUF>';
+		invoicedata = invoicedata + '</summaryNormal>';
+		invoicedata = invoicedata + '<summaryGrossData>';
+		invoicedata = invoicedata + '<invoiceGrossAmount>' + data.invoiceGrossAmount + '.00</invoiceGrossAmount>';
+		invoicedata = invoicedata + '<invoiceGrossAmountHUF>' + data.invoiceGrossAmount + '.00</invoiceGrossAmountHUF>';
+		invoicedata = invoicedata + '</summaryGrossData>';
+	}
+	if(data.invoiceCategory == 'SIMPLIFIED'){
+		invoicedata = invoicedata + '<summarySimplified>';
+		invoicedata = invoicedata + '<vatContent>' + (data.vatContent / 100).toString() + '</vatContent>';
+		invoicedata = invoicedata + '<vatContentGrossAmount>' + data.invoiceGrossAmount + '.00</vatContentGrossAmount>';
+		invoicedata = invoicedata + '<vatContentGrossAmountHUF>' + data.invoiceGrossAmount + '.00</vatContentGrossAmountHUF>';
+		invoicedata = invoicedata + '</summarySimplified>';
+	}
+	invoicedata = invoicedata + '</invoiceSummary>';
 	
 	invoicedata = invoicedata + '</invoiceHead>';
 	
